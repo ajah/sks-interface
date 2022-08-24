b0868f82b7bf39b3013091f034c4167007bae0ed
Patrick Dorosz <patrick.dorosz@gmail.com>
2022-08-24 19:01:21 -0400

    Moves bootstrap loading into public & html file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/bootstrap/css/bootstrap.min.css";
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import Navbar from "./components/common/Navbar";
-import Footer from "./components/common/Footer";
-import HomePage from "./components/homePage";
-import AboutUs from "./components/aboutUs";
-import ResultsPage from "./components/resultsPage";
-import Contact from "./components/contact";
-import OrgPage from "./components/orgPage";
-import ActPage from "./components/actPage";
-import AllActs from "./components/allActs";
-
+import Navbar from './components/common/Navbar'
+import Footer from './components/common/Footer'
+import HomePage from './components/homePage'
+import AboutUs from './components/aboutUs'
+import ResultsPage from './components/resultsPage'
+import Contact from './components/contact'
+import OrgPage from './components/orgPage'
+import ActPage from './components/actPage'
+import AllActs from './components/allActs'
 
 export default function App() {
   return (
@@ -26,8 +23,8 @@ export default function App() {
       <Route exact path="/activities/:id" component={ActPage} />
       <Route exact path="/entities/:id" component={OrgPage} />
       <Route exact path="/activitiesbyent/:ent_id" component={AllActs} />
-      
+
       <Footer />
     </Router>
-  );
+  )
 }
