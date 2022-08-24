995211d0132702c9952a9853f7718782c3cd1af5
Patrick Dorosz <patrick.dorosz@gmail.com>
2022-08-24 18:50:04 -0400

    Simplifies button text

--- src/components/common/BackButton.js
+++ src/components/common/BackButton.js
@@ -6,14 +6,11 @@ export default function BackButton() {
   const searchContext = useContext(SearchContext)
   return (
     <div>
-      {/*   <button className="btn btn-outline-primary" onClick={history.goBack}>
-        Go Back to Results
-      </button> */}
       <Link
         className="btn btn-outline-primary"
         to={`/results?q=${searchContext.searchArray.join('+')}&filter=activity,entity`}
       >
-        Go Back to Results
+        Back to Results
       </Link>
     </div>
   )
