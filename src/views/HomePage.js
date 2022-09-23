aa32adb1ff0b3e0007d77b063e0a01c550fcbd64
Patrick Dorosz <patrick.dorosz@gmail.com>
2022-09-23 13:04:20 -0400

    Clears address bar if redirected to homepage from bad url

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+
+import { useMountEffect } from 'hooks'
 
 import { SearchBar } from 'components/SearchBar'
 import { TypesSection } from 'components/TypesSection'
@@ -9,73 +11,83 @@ import './HomePage.css'
 // import img2 from "./../assets/img/nonprofits/joel-muniz-qvzjG2pF4bE-unsplash.jpg";
 // import img4 from "./../assets/img/nonprofits/tyler-lagalo-ZU94isADXDs-unsplash.jpg";
 
-export default class HomePage extends Component {
-  render() {
-    return (
-      <main className="page landing-page">
-        <section className="portfolio-block block-intro">
-          <div className="container">
-            <div className="p-2 bg-light round-3">
-              <div className="container-fluid pt-5">
-                <div className="row">
-                  <div className="col px-md-5 px-1">
-                    <h1 className=" fw-bold m-4 welcome-h1 text-center">
-                      Sector Knowledge Sharing Hub (Beta)
-                    </h1>
-                    <p className="welcome-p m-4 text-center">
-                      The SKS Hub is a prototype project intended to demonstrate the value
-                      of shared and open knowledge infrastructure for the nonprofit
-                      sector. The Hub collects information from existing data sources
-                      about nonprofit organizations and their activities, and makes it
-                      possible to search, filter, and navigate this information for sector
-                      leaders, advocates, researchers, and practitioners. To use the tool,
-                      simply search the data by choosing keywords related to your
-                      particular interests or focus, and use the filters and controls on
-                      the search page to further refine your results.
-                    </p>
-                    <p className="text-start mb-0">
-                      We explored various ways this prototype could support different
-                      organizations in their work, including:
-                    </p>
-                    <ul className="text-start">
-                      <li>
-                        <p className="fs-5 mb-0">
-                          Helping nonprofit organizations to find other organizations
-                          working in the same focus area within the same geographical
-                          location, in order to connect with them and share resources.
-                        </p>
-                      </li>
-                      <li>
-                        <p className="fs-5 mb-0">
-                          Enabling advocacy organizations to find and segment
-                          organizations that represent the communities they advocate for,
-                          in order to understand the level of funding being received by
-                          these organizations.
-                        </p>
-                      </li>
-                      <li>
-                        <p className="fs-5 mb-0">
-                          Assisting foundations in finding activities being carried out in
-                          their focus area or region, in order to explore funding gaps or
-                          opportunities that are currently not being met in these areas.
-                        </p>
-                      </li>
-                    </ul>
+const HomePage = () => {
+  const { pathname, search, hash } = useLocation()
+  const navigate = useNavigate()
 
-                    <a href="/about-us">
-                      <p>To learn more, read our About Us page.</p>
-                    </a>
-                  </div>
+  useMountEffect(() => {
+    if (pathname === '/' && !search && !hash) return
+
+    navigate('/', {
+      replace: true,
+    })
+  })
+
+  return (
+    <main className="page landing-page">
+      <section className="portfolio-block block-intro">
+        <div className="container">
+          <div className="p-2 bg-light round-3">
+            <div className="container-fluid pt-5">
+              <div className="row">
+                <div className="col px-md-5 px-1">
+                  <h1 className=" fw-bold m-4 welcome-h1 text-center">
+                    Sector Knowledge Sharing Hub (Beta)
+                  </h1>
+                  <p className="welcome-p m-4 text-center">
+                    The SKS Hub is a prototype project intended to demonstrate the value
+                    of shared and open knowledge infrastructure for the nonprofit sector.
+                    The Hub collects information from existing data sources about
+                    nonprofit organizations and their activities, and makes it possible to
+                    search, filter, and navigate this information for sector leaders,
+                    advocates, researchers, and practitioners. To use the tool, simply
+                    search the data by choosing keywords related to your particular
+                    interests or focus, and use the filters and controls on the search
+                    page to further refine your results.
+                  </p>
+                  <p className="text-start mb-0">
+                    We explored various ways this prototype could support different
+                    organizations in their work, including:
+                  </p>
+                  <ul className="text-start">
+                    <li>
+                      <p className="fs-5 mb-0">
+                        Helping nonprofit organizations to find other organizations
+                        working in the same focus area within the same geographical
+                        location, in order to connect with them and share resources.
+                      </p>
+                    </li>
+                    <li>
+                      <p className="fs-5 mb-0">
+                        Enabling advocacy organizations to find and segment organizations
+                        that represent the communities they advocate for, in order to
+                        understand the level of funding being received by these
+                        organizations.
+                      </p>
+                    </li>
+                    <li>
+                      <p className="fs-5 mb-0">
+                        Assisting foundations in finding activities being carried out in
+                        their focus area or region, in order to explore funding gaps or
+                        opportunities that are currently not being met in these areas.
+                      </p>
+                    </li>
+                  </ul>
+
+                  <a href="/about-us">
+                    <p>To learn more, read our About Us page.</p>
+                  </a>
                 </div>
               </div>
             </div>
           </div>
-        </section>
-        <SearchBar />
-        <section>
-          <TypesSection />
-        </section>
-        {/* <section className="portfolio-block">
+        </div>
+      </section>
+      <SearchBar />
+      <section>
+        <TypesSection />
+      </section>
+      {/* <section className="portfolio-block">
           <div className="container">
             <div className="row g-0">
               <div className="col-md-6 col-lg-4 item zoom-on-hover">
@@ -96,7 +108,8 @@ export default class HomePage extends Component {
             </div>
           </div>
         </section> */}
-      </main>
-    )
-  }
+    </main>
+  )
 }
+
+export default HomePage
